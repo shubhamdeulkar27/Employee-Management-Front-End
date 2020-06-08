85f6941ea204fb191f950c8cf2b00fd89beb3faa
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-06-08 12:29:39 +0530

    Added: Register Component

--- employeemanagement/src/App.js
+++ employeemanagement/src/App.js
@@ -2,12 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from './Components/Login'
+import Register from './Components/Register'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <Login/>
+      <Switch>
+          <Route exact path='/Login' component={Login}/>
+          <Route path='/Register' component={Register}/>
+      </Switch>
     </div>
+    </Router>
   );
 }
 
