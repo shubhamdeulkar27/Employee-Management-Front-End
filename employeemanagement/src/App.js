231c868f5122ec23eb35e586b2e235bf26c33c42
shubham <shubhamdeulkar27@gmail.com>
2020-06-09 14:52:21 +0530

    Refactored: Code For Login and Register, Added: AppBar Components

--- employeemanagement/src/App.js
+++ employeemanagement/src/App.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Login from './Components/Login'
-import Register from './Components/Register'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Login from "./Components/Login";
+import Register from "./Components/Register";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Appbar from "./Components/Appbar";
 
 function App() {
   return (
     <Router>
-    <div className="App">
-      <Switch>
-          <Route exact path='/Login' component={Login}/>
-          <Route path='/Register' component={Register}/>
-      </Switch>
-    </div>
+      <div className="App">
+        <Appbar />
+        <br />
+        <Switch>
+          <Route exact path="/Login" component={Login} />
+          <Route path="/Register" component={Register} />
+        </Switch>
+      </div>
     </Router>
   );
 }
--- employeemanagement/src/Components/Register.jsx
+++ employeemanagement/src/Components/Register.jsx
@@ -1,178 +1,175 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
 
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Role: "",
+      EmailId: "",
+      UseerName: "",
+      Password: "",
+    };
 
-class Register extends Component
-{
-    constructor(props)
-    {
-        super(props);
-        this.state={
-            Role:'',
-            EmailId:'',
-            UseerName:'',
-            Password:''
+    this.Role = this.Role.bind(this);
+    this.EmailId = this.EmailId.bind(this);
+    this.UserName = this.UserName.bind(this);
+    this.Password = this.Password.bind(this);
+  }
+
+  Role(event) {
+    this.setState({ Role: event.target.value });
+  }
+
+  EmailId(event) {
+    this.setState({ EmailId: event.target.value });
+  }
+
+  UserName(event) {
+    this.setState({ UserName: event.target.value });
+  }
+
+  Password(event) {
+    this.setState({ Password: event.target.value });
+  }
+
+  Register = () => {
+    axios
+      .post("https://localhost:44315/api/employee/registeruser", {
+        Role: this.state.Role,
+        EmailId: this.state.EmailId,
+        UserName: this.state.UserName,
+        Password: this.state.Password,
+      })
+      .then((json) => {
+        if (json.data.Success === "True") {
+          console.log(json.data.Success);
+          alert("Data Saved Successfully");
+          this.props.history.push("/Login");
+        } else {
+          alert("Data not Saved");
+          debugger;
+          this.props.history.push("/Register");
         }
+      });
+  };
 
-        this.Role = this.Role.bind(this);
-        this.EmailId = this.EmailId.bind(this);
-        this.UserName = this.UserName.bind(this);
-        this.Password = this.Password.bind(this);
-    }
+  render() {
+    const classes = makeStyles((theme) => ({
+      paper: {
+        marginTop: theme.spacing(8),
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      },
+      avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+      },
+      form: {
+        width: "100%", // Fix IE 11 issue.
+        marginTop: theme.spacing(3),
+      },
+      submit: {
+        margin: theme.spacing(3, 0, 2),
+      },
+    }));
 
-    Role(event)
-    {
-        this.setState({Role: event.target.value })
-    }
-
-    EmailId(event)
-    {
-        this.setState({EmailId: event.target.value })
-    }
-
-    UserName(event)
-    {
-        this.setState({UserName: event.target.value })
-    }
-
-    Password(event)
-    {
-        this.setState({Password: event.target.value })
-    }
-
-    Register=()=>{
-        axios.post('https://localhost:44315/api/employee/registeruser', 
-        {
-            Role:this.state.Role,
-            EmailId:this.state.EmailId,
-            UserName:this.state.UserName,
-            Password:this.state.Password
-        })
-        .then(json=>{
-            if(json.data.Success==='True'){
-                console.log(json.data.Success);
-                alert("Data Saved Successfully");
-                this.props.history.push('/Login')
-            }
-            else{
-                alert('Data not Saved');
-                debugger;
-                this.props.history.push('/Register')
-            }  
-        })  
-    }
-
-    render()
-    {
-        const classes = makeStyles((theme) => ({
-            paper: {
-              marginTop: theme.spacing(8),
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            },
-            avatar: {
-              margin: theme.spacing(1),
-              backgroundColor: theme.palette.secondary.main,
-            },
-            form: {
-              width: '100%', // Fix IE 11 issue.
-              marginTop: theme.spacing(3),
-            },
-            submit: {
-              margin: theme.spacing(3, 0, 2),
-            },
-        }));
-
-        return(
-            <Container component="main" maxWidth="xs">
-                <CssBaseline />
-                <div className={classes.paper}>
-                    <Typography component="h1" variant="h4">
-                        Employee Management System
-                    </Typography>
-                    <Typography component="h1" variant="h5">
-                        Sign up
-                    </Typography>
-                    <form className={classes.form} noValidate>
-                        <Grid container spacing={2}>
-                            <Grid item xs={12} sm={6}>
-                                <TextField
-                                autoComplete="Role"
-                                name="Role"
-                                variant="outlined"
-                                required
-                                fullWidth
-                                id="Role"
-                                label="Role"
-                                autoFocus/>
-                            </Grid>
-                            <Grid item xs={12} sm={6}>
-                                <TextField
-                                variant="outlined"
-                                required
-                                fullWidth
-                                id="UserName"
-                                label="Username"
-                                name="UserName"
-                                autoComplete="UserName"/>
-                            </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                variant="outlined"
-                                required
-                                fullWidth
-                                id="EmailId"
-                                label="Email Address"
-                                name="EmailId"
-                                autoComplete="EmailId"/>
-                            </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                variant="outlined"
-                                required
-                                fullWidth
-                                name="Password"
-                                label="Password"
-                                type="Password"
-                                id="Password"
-                                autoComplete="current-password"/>
-                            </Grid>
-                        </Grid>
-                        <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        className={classes.submit}
-                        onClick={this.Register}>
-                            Sign Up
-                        </Button>
-                        <Grid container justify="flex-end">
-                            <Grid item>
-                                <Link href="/Login" variant="body2">
-                                    Already have an account? Sign in
-                                </Link>
-                            </Grid>
-                        </Grid>
-                    </form>
-                </div>
-            </Container>
-        );
-    }
+    return (
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <form className={classes.form} noValidate>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="Role"
+                  name="Role"
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="Role"
+                  label="Role"
+                  autoFocus
+                  onChange={this.Role}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="UserName"
+                  label="Username"
+                  name="UserName"
+                  autoComplete="UserName"
+                  onChange={this.UserName}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="EmailId"
+                  label="Email Address"
+                  name="EmailId"
+                  autoComplete="EmailId"
+                  onChange={this.EmailId}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  name="Password"
+                  label="Password"
+                  type="Password"
+                  id="Password"
+                  autoComplete="current-password"
+                  onChange={this.Password}
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+              onClick={this.Register}
+            >
+              Sign Up
+            </Button>
+            <Grid container justify="flex-end">
+              <Grid item>
+                <Link href="/Login" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+      </Container>
+    );
+  }
 }
 
 export default Register;
